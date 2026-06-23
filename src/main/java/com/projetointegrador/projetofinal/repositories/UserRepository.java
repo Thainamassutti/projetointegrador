9c18ae3067d24c2a9fecd43291b297c6f8a00dbf
Thainamassutti <thainamassutti0202@gmail.com>
2026-06-22 21:38:34 -0300

    Injeção de dependências e inserção de dados

---
+++ src/main/java/com/projetointegrador/projetofinal/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.projetointegrador.projetofinal.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetointegrador.projetofinal.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
