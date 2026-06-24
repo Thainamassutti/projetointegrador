a16814a0cf7b4dc46ab4e9259e07ab2993a5abb0
Thainamassutti <thainamassutti0202@gmail.com>
2026-06-24 20:36:58 -0300

    Entidade ItemPedido |

---
+++ src/main/java/com/projetointegrador/projetofinal/entities/ItemPedido.java
@@ -0,0 +1,84 @@
+package com.projetointegrador.projetofinal.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.projetointegrador.projetofinal.entities.pk.ItemPedidoPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "/tb_item_pedido")
+public class ItemPedido implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private ItemPedidoPK id;
+	
+	private Integer quantidade;
+	private Double price;
+	
+	public ItemPedido() {
+		
+	}
+
+	public ItemPedido(Pedido pedido, Produto produto,Integer quantidade, Double price) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.quantidade = quantidade;
+		this.price = price;
+	}
+	
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
